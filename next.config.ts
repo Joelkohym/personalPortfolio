2dab194cc768867d85a05581bfca8a3407e3460b
Joel Koh <joelkohym@gmail.com>
2026-05-10 10:54:43 +0800

    fix: explicit image src type

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
 	allowedDevOrigins: ["192.168.56.1"],
 	typescript: {
 		// This allows the build to complete even if there are type errors
-		ignoreBuildErrors: true,
 	},
 };
 
